276182b6686adf432b917b7aa010c19f26e068f9
raflinoob132 <limandijaya@gmail.com>
2025-06-21 15:05:18 +0700

    Refactor security check logic for improved clarity and maintainability

--- logic/securityCheck.js
+++ logic/securityCheck.js
@@ -1,8 +1,15 @@
 const { db } = require("../config/firebase");
 const { moveServoAndTakePhoto } = require("./servoHandler");
 const { sendTelegramMessage } = require("../telegram/telegramUtils");
+let isChecking = false;
 
 async function checkSecurity() {
+  if (isChecking) {
+    console.log("Proses pemeriksaan keamanan sedang berlangsung.");
+    return;
+  }
+  isChecking = true;
+try{
   const snapshot = await db.ref("JarakKeamanan").once("value");
   const sensors = snapshot.val();
 
@@ -28,7 +35,7 @@ async function checkSecurity() {
 
     if (pirValue === 1 && !flagAktif) {
       adaAncaman = true;
-      const servoCommand = servoCommands[sensor] || "0,0";
+      const servoCommand = servoCommands[sensor];
       const pesan = `Ancaman terdeteksi di ${sensor} (PIR aktif). Menggerakkan servo dengan command ${servoCommand}.`;
       console.log(pesan);
       await db.ref(`AncamanKeamananAktif/${sensor}`).set(true);
@@ -56,6 +63,10 @@ async function checkSecurity() {
   if (!adaAncaman) {
     console.log("Tidak ada ancaman keamanan terdeteksi.");
   }
+} catch (err) {
+    console.error("Error saat cek keamanan:", err);
+  } finally {
+    isChecking = false;
+  }
 }
-
 module.exports = { checkSecurity };
